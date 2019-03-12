f124a506ab981c8cb4a3348c6e83b8894039f570
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-03-12 15:16:02 +0000

    Added ability to close modal when history is empty

--- client/app/components/modal/link-modal.js
+++ client/app/components/modal/link-modal.js
@@ -14,26 +14,32 @@ export default class LinkModal extends React.Component {
     }
   }
 
+  close = () => {
+    if (this.props.history.length > 1) {
+      this.props.history.goBack()
+    } else {
+      this.props.history.push(this.props.match.path)
+    }
+  }
+
   render () {
     return (
       <div className={this.getClassName()}>
-        <Route render={({ location, history }) => (
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              classNames='animation'
-              timeout={300}
-              unmountOnExit>
-              <Switch location={location}>
-                <Route path={this.props.path}>
-                  <Modal close={history.goBack}>
-                    {this.props.children}
-                  </Modal>
-                </Route>
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )} />
+        <TransitionGroup>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames='animation'
+            timeout={300}
+            unmountOnExit>
+            <Switch location={this.props.location}>
+              <Route path={this.props.path}>
+                <Modal close={this.close}>
+                  {this.props.children}
+                </Modal>
+              </Route>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     )
   }
@@ -42,5 +48,8 @@ export default class LinkModal extends React.Component {
 LinkModal.propTypes = {
   className: PropTypes.string,
   path: PropTypes.string,
-  children: PropTypes.node
+  children: PropTypes.node,
+  history: PropTypes.object,
+  match: PropTypes.object,
+  location: PropTypes.object
 }
--- client/app/modals/test-modal.js
+++ client/app/modals/test-modal.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Route } from 'react-router-dom'
 
 import LinkModal from '../components/modal/link-modal'
 
 export default class TestModal extends React.Component {
   render () {
     return (
-      <LinkModal className='test-modal' path={this.props.path}>
-        <h1>Test Modal</h1>
-      </LinkModal>
+      <Route render={(props) => (
+        <LinkModal {...props} className='test-modal' path={this.props.path}>
+          <h1>Test Modal</h1>
+        </LinkModal>
+      )} />
     )
   }
 }
