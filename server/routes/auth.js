8adb1971ab3ae5b5d9d20248ccfae39f1b94c3c8
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-03-15 11:02:23 +0000

    Checking if a user has entered a email and password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,27 +2,40 @@ var express = require('express')
 var router = express.Router()
 
 // Models
-const User = require('../models/User')
+// const User = require('../models/User')
 // const UserSession = require('../models/UserSession')
 
 /*
 * Sign up
 */
-// router.post('/signup', (req, res, next) => {
+router.post('/signup', (req, res, next) => {
+  const { body } = req
+  const { password } = body
+  let { email } = body
 
-// })
+  if (!email) {
+    return res.send({
+      success: false,
+      message: 'Error: Missing email'
+    })
+  }
+
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Missing password'
+    })
+  }
+
+  email = email.toLowerCase()
+})
 
 /*
 * Login
 */
-router.get('/login', (req, res, next) => {
-  User.findAll()
-    .then(user => {
-      console.log(user)
-      res.sendStatus(200)
-    })
-    .catch(err => console.log(err))
-})
+// router.get('/login', (req, res, next) => {
+
+// })
 
 /*
 * Verify User
