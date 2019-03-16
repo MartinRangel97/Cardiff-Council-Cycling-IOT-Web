dba34ac39f2560738c01356d6f39a4767f0c9086
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-03-16 17:15:54 +0000

    Added a Mapbox map

--- client/app/components/map/map-view.js
+++ client/app/components/map/map-view.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import mapboxgl from 'mapbox-gl'
 
 import Searchbar from './searchbar'
 import OverlayPicker from './overlay-picker'
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXRoYW5wZXRlcmNvbGUiLCJhIjoiY2p0YmkzanVwMGtyNTN5bzNydTNpYjB2OSJ9.ac1RTVcnsO8Ek-rgVeQe3g'
 
 export default class MapView extends React.Component {
   constructor (props) {
@@ -9,6 +12,17 @@ export default class MapView extends React.Component {
     this.state = {
       selectedOverlay: 'none'
     }
+  }
+
+  componentDidMount () {
+    // Public Style URL:
+    // https://api.mapbox.com/styles/v1/jonathanpetercole/cjtb9gdix19sd1fmy23x766v3.html?fresh=true&title=true&access_token=pk.eyJ1Ijoiam9uYXRoYW5wZXRlcmNvbGUiLCJhIjoiY2p0YWhqaTRrMGFydjQzcWQ1NWR5aTk3dCJ9.V7HyWXQG5lpWtgk-17y6yw#13.5/51.480233/-3.152327/0
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/jonathanpetercole/cjtb9gdix19sd1fmy23x766v3',
+      center: [-3.175559, 51.480802],
+      zoom: 13.75
+    })
   }
 
   changeSelectedOverlay = (selection) => {
@@ -27,9 +41,7 @@ export default class MapView extends React.Component {
             <OverlayPicker selected={this.state.selectedOverlay} onChange={this.changeSelectedOverlay} />
           </div>
         </div>
-        <div className='map'>
-          Map
-        </div>
+        <div className='map' ref={element => (this.mapContainer = element)} />
       </div>
     )
   }
