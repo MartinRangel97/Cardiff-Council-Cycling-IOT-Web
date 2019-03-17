3bf2b19c946ea0750e54ce6fe162ac7cbef9d805
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-03-17 10:17:16 +0000

    Added body-parser

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
 // Database
 const database = require('./database')
@@ -47,6 +48,8 @@ app.set('view engine', 'pug')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
 // Routes
