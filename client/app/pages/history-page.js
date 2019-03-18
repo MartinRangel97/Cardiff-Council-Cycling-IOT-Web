17fd8e85232d301a45fb193dd4e1a2a7b2d79e44
Gareth Beddis <garethbeddis@gmail.com>
2019-03-18 19:20:19 +0000

    Implemented rc-calendar for history date picker

--- client/app/pages/history-page.js
+++ client/app/pages/history-page.js
@@ -3,6 +3,8 @@ import React from 'react'
 import SidebarPage from '../components/sidebar/sidebar-page'
 import Section from '../components/common/section'
 import Card from '../components/common/card'
+import Calendar from 'rc-calendar'
+import 'rc-calendar/assets/index.css'
 
 export default class HistoryPage extends React.Component {
   render () {
@@ -10,7 +12,19 @@ export default class HistoryPage extends React.Component {
       <SidebarPage title='History'>
         <Section title='Hello World'>
           <Card>
-            <h1>History Will Go Here</h1>
+            <Calendar
+              showWeekNumber={false}
+              // locale={cn ? zhCN : enUS}
+              // defaultValue={now}
+              // disabledTime={disabledTime}
+              showDateInput={false}
+              showToday={false}
+              showOk={false}
+              // timePicker={timePickerElement}
+              // onChange={onStandaloneChange}
+              // disabledDate={disabledDate}
+              // onSelect={onStandaloneSelect}
+            />
           </Card>
         </Section>
       </SidebarPage>
