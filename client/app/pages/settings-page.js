ebeb15394f8f5616415368cc5a06b2879c73395f
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-03-25 10:34:38 +0000

    Added base settings and divider

--- client/app/pages/settings-page.js
+++ client/app/pages/settings-page.js
@@ -5,31 +5,40 @@ import Page from '../components/page/page'
 import Section from '../components/common/section'
 import Card from '../components/common/card'
 import Button from '../components/common/button'
-import IconButton from '../components/common/icon-button'
 import Toggle from '../components/common/toggle'
-
-import IconBike from './settings-page/icons/bike.svg'
+import Divider from '../components/common/divider'
 
 export default class SettingsModal extends React.Component {
   render () {
     return (
       <Page className='settings-modal' title='Settings' path={this.props.path} canGoBack>
-        <Section title='Text Buttons'>
+        <Section title='General'>
           <Card>
-            <Button text='Standard Button' />
-            <Button text='Danger Button' danger />
+            <h1>Language</h1>
+            <h2>Select your Language / Dewiswch eich iaith</h2>
           </Card>
         </Section>
-        <Section title='Icon Button'>
+        <Section title='Privacy'>
           <Card>
-            <IconButton icon={IconBike} />
-          </Card>
-        </Section>
-        <Section title='Toggles'>
-          <Card>
+            <h1>Share Readings Publicly</h1>
+            <h2>Your readings are currently shown on the public map.</h2>
+            <h2>These readings are anonymous and your trips are not public.</h2>
             <Toggle />
-            <br />
-            <Toggle defaultChecked />
+          </Card>
+        </Section>
+        <Section title='Account'>
+          <Card>
+            <h1>Email</h1>
+            <h2>Change your account email.</h2>
+            <Button text='Change Email' />
+            <Divider />
+            <h1>Password</h1>
+            <h2>Change your account Password.</h2>
+            <Button text='Change Password' />
+            <Divider />
+            <h1>Delete Account</h1>
+            <h2>Careful, this cannot be undone.</h2>
+            <Button text='Delete Account' danger />
           </Card>
         </Section>
       </Page>
