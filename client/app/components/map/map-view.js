5e12e6f7ebcd4399ca19664e4de8a00068944f2a
Gareth Beddis <garethbeddis@gmail.com>
2019-03-26 15:25:06 +0000

    Removed heatmap layer in place of circle-points that reflect AQI

--- client/app/components/map/map-view.js
+++ client/app/components/map/map-view.js
@@ -36,62 +36,12 @@ export default class MapView extends React.Component {
         data: '/static/trees.geojson'
       })
       // add heatmap layer here
-      this.map.addLayer({
-        id: 'trees-heat',
-        type: 'heatmap',
-        source: 'trees',
-        maxzoom: 15,
-        paint: {
-          // increase weight as diameter breast height increases
-          'heatmap-weight': {
-            property: 'dbh',
-            type: 'exponential',
-            stops: [
-              [1, 0],
-              [62, 1]
-            ]
-          },
-          // increase intensity as zoom level increases
-          'heatmap-intensity': {
-            stops: [
-              [11, 1],
-              [15, 3]
-            ]
-          },
-          // assign color values be applied to points depending on their density
-          'heatmap-color': [
-            'interpolate',
-            ['linear'],
-            ['heatmap-density'],
-            0, 'rgba(236,222,239,0)',
-            0.2, 'rgb(208,209,230)',
-            0.4, 'rgb(166,189,219)',
-            0.6, 'rgb(103,169,207)',
-            0.8, 'rgb(28,144,153)'
-          ],
-          // increase radius as zoom increases
-          'heatmap-radius': {
-            stops: [
-              [11, 15],
-              [15, 20]
-            ]
-          },
-          // decrease opacity to transition into the circle layer
-          'heatmap-opacity': {
-            default: 1,
-            stops: [
-              [14, 1],
-              [15, 0]
-            ]
-          }
-        }
-      }, 'road-label')
+
       // add circle layer here
       this.map.addLayer({
         id: 'trees-point',
         type: 'circle',
         source: 'trees',
-        minzoom: 14,
         paint: {
           // increase the radius of the circle as the zoom level and dbh value increases
           'circle-radius': {
@@ -108,20 +58,22 @@ export default class MapView extends React.Component {
             property: 'dbh',
             type: 'exponential',
             stops: [
-              [0, 'rgba(236,222,239,0)'],
-              [10, 'rgb(236,222,239)'],
-              [20, 'rgb(208,209,230)'],
-              [30, 'rgb(166,189,219)'],
-              [40, 'rgb(103,169,207)'],
-              [50, 'rgb(28,144,153)'],
-              [60, 'rgb(1,108,89)']
+              // TODO: change stops to reflect AQI
+              [0, 'rgba(0, 228, 0, 0)'],
+              [10, 'rgb(0, 228, 0)'], // green - good
+              [20, 'rgb(255, 255, 0)'], // yellow - moderate
+              [30, 'rgb(255, 126, 0)'], // orange - unhealthy for sensitive groups
+              [40, 'rgb(255, 0, 0)'], // red - unhealthy
+              [50, 'rgb(143, 63, 151)'], // purple - very unhealthy
+              [60, 'rgb(143, 63, 151)'] // maroon - hazardous
             ]
           },
           'circle-stroke-color': 'white',
           'circle-stroke-width': 1,
           'circle-opacity': {
             stops: [
-              [14, 0],
+              // Fade circles at zoom level
+              [14, 1],
               [15, 1]
             ]
           }
@@ -132,6 +84,14 @@ export default class MapView extends React.Component {
     this.map.on('click', (event) => {
       this.props.onMapClick(event)
     })
+
+    // Uncaught TypeError: Cannot read property 'on' of undefined
+    // this.map.on('click', 'trees-point', function (e) {
+    //   new mapboxgl.Popup()
+    //     .setLngLat(e.features[0].geometry.coordinates)
+    //     .setHTML('<b>DBH:</b> ' + e.features[0].properties.dbh)
+    //     .addTo(this.map)
+    // })
   }
 
   changeSelectedOverlay = (selection) => {
