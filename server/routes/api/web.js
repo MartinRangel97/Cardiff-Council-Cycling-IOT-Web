0060c21c81d6696dfac28e3d7e72aea160977125
Martin Rangel <rangelm@cardiff.ac.uk>
2019-03-29 17:45:42 +0000

    created a get request to get average NO2 readings

--- server/routes/api/web.js
+++ server/routes/api/web.js
@@ -5,47 +5,53 @@ const Serializer = require('sequelize-to-json')
 const { Op } = require('sequelize')
 var GeoJSON = require('geojson')
 const moment = require('moment')
-// const moment = require('moment')
 
 // Example Route
 router.get('/example', function (req, res, next) {
   res.send({ example: 'JSON' })
 })
 
-router.get('/noiseAverage', function (req, res, next) {
-  const scheme = {
-    include: ['@all'],
-    exclude: ['@pk', '@fk'],
-    assoc: {
-      include: ['dBReading', 'NO2Reading', 'PM10Reading', 'PM25Reading', 'timeTaken', 'longitude', 'latitude']
-    }
+// sets a template for the JSON
+const scheme = {
+  include: ['@all'],
+  exclude: ['@pk', '@fk'],
+  assoc: {
+    include: ['dBReading', 'NO2Reading', 'PM10Reading', 'PM25Reading', 'timeTaken', 'longitude', 'latitude']
   }
+}
 
+router.get('/noiseAverage', function (req, res, next) {
   var noiseAve = 0
 
   database.getDatabase().measurement.findAll({
-    // TODO add a WHERE condition to get all points in cardiff
+    // TODO add a WHERE condition to get all points in cardiff in the last 24 hrs
   }).then(async function (posts) {
     let postsAsJSON = Serializer.serializeMany(posts, database.getDatabase().measurement, scheme)
     postsAsJSON.forEach((reading) => {
       noiseAve += reading.dBReading
     })
     noiseAve = noiseAve / postsAsJSON.length
-    console.log('Hello there')
     res.send(noiseAve.toString())
+  })
+})
+
+router.get('/NO2Average', function (req, res, next) {
+  var NO2Ave = 0
+
+  database.getDatabase().measurement.findAll({
+    // TODO add a WHERE condition to get all points in cardiff
+  }).then(async function (posts) {
+    let postsAsJSON = Serializer.serializeMany(posts, database.getDatabase().measurement, scheme)
+    postsAsJSON.forEach((reading) => {
+      NO2Ave += reading.NO2Reading
+    })
+    NO2Ave = NO2Ave / postsAsJSON.length
+    res.send(NO2Ave.toString())
   })
 })
 
 router.get('/measurements', function (req, res, next) {
   // sets a template for the JSON
-  const scheme = {
-    include: ['@all'],
-    exclude: ['@pk', '@fk'],
-    assoc: {
-      include: ['dBReading', 'NO2Reading', 'PM10Reading', 'PM25Reading', 'timeTaken', 'longitude', 'latitude']
-    }
-  }
-
   database.getDatabase().measurement.findAll({
     where: {
       timeTaken: {
