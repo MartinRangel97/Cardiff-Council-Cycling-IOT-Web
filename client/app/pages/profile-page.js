e208d792224dfba695c01b39b15f5ea86d43743c
Gareth Beddis <garethbeddis@gmail.com>
2019-03-30 17:06:31 +0000

    Added hard coded template for profile page

--- client/app/pages/profile-page.js
+++ client/app/pages/profile-page.js
@@ -7,6 +7,10 @@ import SidebarPage from '../components/sidebar/sidebar-page'
 import Section from '../components/common/section'
 import Card from '../components/common/card'
 
+import IconAirPollution from './explore-page/icons/air-pollution.svg'
+import IconNoise from './explore-page/icons/noise.svg'
+import IconBike from './settings-page/icons/bike.svg'
+
 export default class ProfilePage extends React.Component {
   render () {
     return (
@@ -23,10 +27,59 @@ export default class ProfilePage extends React.Component {
         } />
         <Route path={`${this.props.match.path}/`} render={() =>
           <SidebarPage title='Profile'>
+            <Section title='Your Statistics'>
+              <Card className='average' link={``}>
+                <IconBike className='icon' />
+                <div className='details'>
+                  <h1>Total Distance Travelled</h1>
+                  <span className='value'>23 Miles</span>
+                </div>
+              </Card>
+              <Card className='average' link={``}>
+                <IconAirPollution className='icon' />
+                <div className='details'>
+                  <h1>Average Air Pollution Exposure</h1>
+                  <span className='value'>Moderate</span>
+                </div>
+              </Card>
+              <Card className='average' link={``}>
+                <IconNoise className='icon' />
+                <div className='details'>
+                  <h1>Average Noise Pollution Exposure</h1>
+                  <span className='value'>25 dBA</span>
+                </div>
+              </Card>
+            </Section>
             <Section title='Your Trips'>
-              <Card link={`${this.props.match.path}/subpage`}>
-                <h1>Open Subpage Example</h1>
-                <h2>Click here to open the subpage example.</h2>
+              <Card className='average' link={``}>
+                <div className='date'>
+                  <h1>March</h1>
+                  <span>30</span>
+                </div>
+                <div className='details'>
+                  <h1>6:45 - 7:30</h1>
+                  <span className='value'>3 Miles</span>
+                </div>
+              </Card>
+              <Card className='average' link={``}>
+                <div className='date'>
+                  <h1>January</h1>
+                  <span>16</span>
+                </div>
+                <div className='details'>
+                  <h1>10:32 - 11:02</h1>
+                  <span className='value'>1 Mile</span>
+                </div>
+              </Card>
+              <Card className='average' link={``}>
+                <div className='date'>
+                  <h1>March</h1>
+                  <span>02</span>
+                </div>
+                <div className='details'>
+                  <h1>9:35 - 11:58</h1>
+                  <span className='value'>5 Miles</span>
+                </div>
               </Card>
             </Section>
           </SidebarPage>
