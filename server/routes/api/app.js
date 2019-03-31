95d376d8dc1b54cdff818bd077019b1533c5c252
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-03-31 16:49:41 +0100

    Added a placeholder journey sync route

--- server/routes/api/app.js
+++ server/routes/api/app.js
@@ -7,7 +7,14 @@ router.get('/example', function (req, res, next) {
 })
 
 router.post('/sync/journeys', (req, res, next) => {
-  res.send([{ example: 'JSON' }])
+  // Get the journeys from the request body
+  let journeys = req.body
+  // Loop through the journeys, getting changing the sync status and setting the remoteId
+  for (let i = 0; i < journeys.length; i++) {
+    journeys[i].Synced = true
+  }
+  // Return the new journeys
+  res.send(journeys)
 })
 
 module.exports = router
