5bfd85f3285147f6b223dfa1ec09b48a96425c6c
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-04-01 17:21:37 +0100

    Split the syncing logic into a service

--- server/routes/api/app.js
+++ server/routes/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const database = require('../../database')
+const syncService = require('../../services/sync/syncService')
 
 // Example Route
 router.get('/example', function (req, res, next) {
@@ -9,49 +9,16 @@ router.get('/example', function (req, res, next) {
 })
 
 router.post('/sync/journeys', async (req, res, next) => {
-  // Get the journeys from the request body
-  let journeys = req.body
-  // Loop through the journeys
-  for (let i = 0; i < journeys.length; i++) {
-    // Create the journey in the database
-    // TODO: Add the current users ID
-    await database.getDatabase().journey.create({
-      userId: 1
-    }).then((newJourney) => {
-      // Set the new journey details
-      journeys[i].Synced = true
-      journeys[i].RemoteId = newJourney.id
-    })
-  }
-  // Return the new journeys
-  res.send(journeys)
+  // Sync and return the new journeys
+  // TODO: Add the current users ID
+  let syncedJourneys = await syncService.syncJourneys(1, req.body)
+  res.send(syncedJourneys)
 })
 
 router.post('/sync/readings', async (req, res, next) => {
-  // The app doesn't need a remote ID for readings, so we just return a list of the synced ID's
-  // Get the readings from the request body
-  let readings = req.body
-  // Prepare an array to hold the synced reading IDs
-  let syncedReadings = []
-  // Loop through the readings
-  for (var reading of readings) {
-    // Create the reading in the database
-    // TODO: Add the current users ID
-    await database.getDatabase().reading.create({
-      userId: 1,
-      journeyId: reading.JourneyRemoteId,
-      dBReading: reading.NoiseReading,
-      NO2Reading: reading.No2Reading,
-      PM10Reading: reading.PM10Reading,
-      PM25Reading: reading.PM25Reading,
-      timeTaken: reading.TimeTaken,
-      longitude: reading.Longitude,
-      latitude: reading.Latitude
-    }).then(() => {
-      syncedReadings.push(reading.id)
-    })
-  }
-  // Return the new journeys
+  // Sync and return the new journeys
+  // TODO: Add the current users ID
+  let syncedReadings = await syncService.syncReadings(1, req.body)
   res.send(syncedReadings)
 })
 
