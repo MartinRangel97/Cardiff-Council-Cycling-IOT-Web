967df4b94d4441f068884945718b703740b4162e
Martin Rangel <rangelm@cardiff.ac.uk>
2019-04-03 02:24:35 +0100

    Feature AQI in journey-subpage is now displaying

--- client/app/pages/profile-page/journey-subpage.js
+++ client/app/pages/profile-page/journey-subpage.js
@@ -15,9 +15,9 @@ export default class JourneySubpage extends React.Component {
     super(props)
     this.state = {
       dBA: 0,
-      NO2: 0,
-      PM10: 0,
-      PM25: 0
+      no2: 0,
+      pm10: 0,
+      pm25: 0
     }
   }
 
@@ -27,23 +27,47 @@ export default class JourneySubpage extends React.Component {
   }
 
   componentWillMount () {
-    this.getJourneyReadings(1, this.getJourneyId())
-    this.props.getAirQualityIndex(this.state.no2, this.state.pm25, this.state.pm10, false)
+    this.getJourneyReadings(1, this.getJourneyId()).then(() => {
+      console.log(this.state)
+      this.props.getAirQualityIndex(this.state.no2, this.state.pm25, this.state.pm10, false)
+    })
   }
+
+  componentDidMount () {}
+
+  // componentWillReceiveProps (nextProp) {
+  //   let curPropAverages = {
+  //     pm10: this.props.pm10,
+  //     pm25: this.props.pm25,
+  //     no2: this.props.no2,
+  //     dBA: this.props.dBA
+  //   }
+  //   let nextPropAverages = {
+  //     pm10: nextProp.pm10,
+  //     pm25: nextProp.pm25,
+  //     no2: nextProp.no2,
+  //     dBA: nextProp.dBA
+  //   }
+  //   if (this.props !== nextProp) {
+  //     nextProp.getAirQualityIndex(nextProp.no2, nextProp.pm25, nextProp.pm10, false)
+  //   }
+  // }
 
   componentDidUpdate () {}
 
-  componentWillUnmount () {}
+  componentWillUnmount () {
+    // this.props.getAirQualityIndex(this.state.no2, this.state.pm25, this.state.pm10, false)
+  }
 
   getJourneyReadings = (userId, journeyId) => {
-    axios.get('/api/web/user/' + userId + '/journeys/' + journeyId + '/measurements/averages')
+    return axios.get('/api/web/user/' + userId + '/journeys/' + journeyId + '/measurements/averages')
       .then((response) => {
         if (response.data !== 'NaN') {
           this.setState({
             dBA: response.data.dBA.toFixed(0),
-            NO2: response.data.NO2.toFixed(0),
-            PM10: response.data.PM10.toFixed(0),
-            PM25: response.data.PM25.toFixed(0)
+            no2: response.data.NO2.toFixed(0),
+            pm10: response.data.PM10.toFixed(0),
+            pm25: response.data.PM25.toFixed(0)
           })
         }
       })
@@ -67,15 +91,15 @@ export default class JourneySubpage extends React.Component {
             <div className='pill-container'>
               <div className='pill'>
                 <h2>NO2</h2>
-                <span>{this.state.NO2} µg/m³</span>
+                <span>{this.state.no2} µg/m³</span>
               </div>
               <div className='pill'>
                 <h2>PM2.5</h2>
-                <span>{this.state.PM25} µgm-3</span>
+                <span>{this.state.pm25} µgm-3</span>
               </div>
               <div className='pill'>
                 <h2>PM10</h2>
-                <span>{this.state.PM10} µg/m³</span>
+                <span>{this.state.pm10} µg/m³</span>
               </div>
             </div>
           </Card>
